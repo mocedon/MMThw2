e22eba396aa321920bb7a2ddbed87e20f4f98ba4
mocedon <49566776+mocedon@users.noreply.github.com>
2019-05-07 13:11:13 +0300

    Finished user.h file

--- HomeWork2/user.h
+++ HomeWork2/user.h
@@ -1,2 +1,32 @@
 #pragma once
 // user header file
+#ifndef _USER_H
+#define _USER_H
+
+#include "def.h"
+
+typedef struct _user {
+
+	char* name ;
+	node* friend_list ;
+	int friend_num ;
+
+} user;
+
+user* createUser(char* newName);
+
+void deleteUser(user* self);
+
+Result addFriend(user* self , char* add);
+
+Result removeFriend(user* self, char* remove);
+
+char* getName(user* self);
+
+node* getFriendList(user* self);
+
+int getFriendNum(user* self);
+
+void printUser(user* self);
+
+#endif
